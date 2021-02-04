f72e1e5fa649c20e58683981f181aeec9f920d2b
ferluminax <ferluminax@gmail.com>
2021-02-04 11:53:52 +0700

    cleansing

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-var _ = require('lodash');
-console.log(_);
-
-var arr = [1,2,3,4,5,6];
-console.log("answer: ", _.without(arr, 1));
-
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
